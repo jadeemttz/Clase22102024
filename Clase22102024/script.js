ae71f500e53f9465c3d117eaedf57561c70651f7
Martínez Hernández Jade Ivet <160188661+jadeemttz@users.noreply.github.com>
2024-10-24 16:45:36 -0600

    Update script.js

--- Clase22102024/script.js
+++ Clase22102024/script.js
@@ -1,1 +1,21 @@
 alert ("hola")
+//1 definimos función normal
+function hola(a,b){
+    return a+b;
+}
+
+console.log(hola(3,5));
+
+//2 convertir a función flecha
+const suma = (a,b) =>{
+    return a+b;
+}
+console.log(suma(3,5));
+
+//3 Simplificar
+const suma2 = (a,b) => a+b;
+console.log(suma(5,5));
+
+//4 solo un parametro
+const cuadrado = x => x*x;
+console.log(cuadrado(5));
